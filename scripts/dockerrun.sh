ce2731493c8bba7edf3fcdd9d83f15d08e9cb064
Geetha699 <130438294+Geetha699@users.noreply.github.com>
2023-06-14 12:37:07 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,2 +1,3 @@
 sudo docker pull geethashreems56/dockerjenkinsdemo:javaapp-v1
+sudo rm -f javaapplicationcontainer
 sudo docker run --name javaapplicationcontainer -itd -p 8080:8080 geethashreems56/dockerjenkinsdemo:javaapp-v1
